ef6d574d4d07ee3c7946e64a93910cd79edaf8bf
Jenny Chen <jenny.chen@centraltrillion.com>
2016-07-10 16:54:37 +0800

    add reset button

--- practice_4_mondrian/script.js
+++ practice_4_mondrian/script.js
@@ -8,4 +8,8 @@ $(document).ready(function(){
   $(".box").click(function(){
     $(this).css("background-color", currentColor);
   });
+
+  $("h2").click(function(){
+    $(".box").css("background-color", "white");
+  });
 });
