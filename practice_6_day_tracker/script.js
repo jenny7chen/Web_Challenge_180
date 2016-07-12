94c62835153bb294e560ed714d7676d4836e3cce
Jenny Chen <jennychen@chenwanqide-Mac-mini.local>
2016-07-12 10:23:15 +0800

    add practice 6 day tracker: get day of the week

---
+++ practice_6_day_tracker/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  resetDayColor();
+  setTodayColor();
+});
+
+function resetDayColor(){
+  $(".weekday").css("color", "black");
+}
+
+function setTodayColor(){
+  var date = new Date();
+  var weekday = date.getDay();
+  $("#" + weekday).css("color", "#ffe6f2");
+  var todayText = $("#" + weekday).text();
+  $("#result").text("It's " + todayText + "!");
+}
