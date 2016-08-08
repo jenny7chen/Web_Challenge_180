d4ffcd8376c9ba2f5b3fdbcefc7b83cfff1740af
Jenny Chen <jennychen@chenwanqide-Mac-mini.local>
2016-08-08 18:05:35 +0800

    add gulden image , adjust scene story

--- practice_22_happy_birthday/js/scene4.js
+++ practice_22_happy_birthday/js/scene4.js
@@ -1,9 +1,12 @@
 function startScene4(){
   var story = ["沒想到走過了烏龜的傳送門, 你瞬間就跑到了骨爾丹的家", "真是太棒了!感嘆烏龜!讚嘆烏龜!", "不過想進骨爾丹的家也不是什麼容易的事", "人家可是率領了整個燃搔軍團呢!", "門上的鎖看起來好複雜><, 該怎麼辦呢?"];
+  var endingText = ["哎呀呀～看看那張帥臉", "小坑第一帥被搶走也沒什麼好說的吧！", "你確定你要跟他搶稱號不回家洗洗睡？"];
+  var yesEnd = "真不要臉，好吧你進去吧！";
+  var noEnd = "算你識相";
   var currentStoryText = 0;
   var timerId = 0;
   var container = document.createElement("div");
-  var puzzles = ["pie_p"];
+  var puzzles = ["_"];
   var puzzleWidth = [3];
   var currentPuzzle = 0;
 
@@ -58,7 +61,7 @@ function startScene4(){
       var id = puzzle[i];
       image.id = id;
       image.classList.add(id);
-      image.setAttribute("src", "../image/puzzle/" + puzzle[i] + ".jpg");
+      image.setAttribute("src", "../image/character/guldan/guldan" + puzzle[i] + ".jpg");
       div.appendChild(image);
       $("#puzzle_container").append(div);
     }
@@ -120,9 +123,41 @@ function startScene4(){
       $("#button_text").off("click");
       startSoundEffect("correct_answer", false);
       setTimeout(function(){
-        onSceneEnd(4);
+        $("#puzzle_container").remove();
+        ending();
       }, 2000);
     }
+  }
+
+  function ending() {
+    $(".start_area").css("text-align", "center");
+    $(".start_area").prepend(story_texts);
+    currentStoryText = 0;
+    $(story_texts).text(endingText[0]);
+    timerId = setInterval(function(){
+      if(currentStoryText == endingText.length - 1){
+        clearInterval(timerId);
+        currentStoryText = -1;
+        $(".start_area").append("<div id=\"scene4_button\"><h3 id=\"button_text1\">我才帥，讓我進去</h3></div>");
+        $(".start_area").append("<div id=\"scene4_button\"><h3 id=\"button_text2\">好吧，讓我冷靜幾秒想想</h3></div>");
+        $("#button_text1").click(function(){
+          $("#button_text1").off("click");
+          $("#button_text2").off("click");
+          $(story_texts).text(yesEnd);
+          setTimeout(function(){
+            onSceneEnd(4);
+          }, 2000);
+        });
+        $("#button_text2").click(function(){
+          $(story_texts).text(noEnd);
+          setTimeout(function(){
+            $(story_texts).text("你確定你要跟他搶稱號不回家洗洗睡？");
+          }, 2000);
+        });
+      }
+      currentStoryText++;
+      $(story_texts).text(endingText[currentStoryText]);
+    }, 3500);
   }
 
   /*Fisher-Yates (aka Knuth) Shuffle.
